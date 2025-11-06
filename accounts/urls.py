cf39b46ca6e9636e02bb1876bdb903c030ec40df
imranme <tusharimranme0@gmail.com>
2025-11-06 06:32:35 +0600

    update login part

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,9 +6,11 @@ urlpatterns = [
     path('verify-otp/', views.VerifyOTPView.as_view()), #phone verify not working
     path('login/', views.UserLoginView.as_view()), #done
     path('change-password/', views.ChangePasswordView.as_view()), #not working 
-    path('forgot-password/', views.ForgotPasswordView.as_view()), #
-    path('reset-password/', views.ResetPasswordView.as_view()),
-    path('profile/', views.UserProfileView.as_view()),
-    path('delete-account/', views.DeleteAccountView.as_view()),
-    path('confirm-delete/', views.ConfirmDeleteView.as_view()),
+    path('forgot-password/', views.ForgotPasswordView.as_view()), #error
+    path('reset-password/', views.ResetPasswordView.as_view()), #done
+    path('profile/', views.UserProfileView.as_view()),#done
+    path('delete-account/', views.DeleteAccountView.as_view()), #error
+    path('confirm-delete/', views.ConfirmDeleteView.as_view()), #NOT WORKING
 ]
+
+
